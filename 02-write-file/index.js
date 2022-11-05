612cf4169593b22615f26c702564617477e82c06
Recomagnesius <buzhor.danyl@gmail.com>
2022-11-05 18:05:11 -0500

    feat: finish 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
 
-const outputFile = path.resolve(__dirname, "file.txt");
+const fileName = "file.txt";
+const outputFile = path.join(__dirname, fileName);
 
 let rl = readline.createInterface({
   input: process.stdin,
@@ -10,19 +11,22 @@ let rl = readline.createInterface({
   prompt:
     "Hello! Please enter information that you want to record in the file\n",
 });
-fs.writeFile(path.resolve(__dirname, "file.txt"), "", (err) => {
+const parting = "See you later!";
+
+fs.writeFile(path.resolve(__dirname, fileName), "", (err) => {
   if (err) {
     throw err;
   }
   rl.prompt();
 });
-process.on("SIGINT", (err) => {
+rl.on("SIGINT", (err) => {
   if (err) throw err;
-  console.log("enter ctrl + c");
+  console.log(parting);
+  rl.close();
 });
 rl.on("line", (input) => {
   if (input.toLowerCase() == "exit") {
-    console.log("See you later!");
+    console.log(parting);
     rl.close();
   } else {
     fs.appendFile(outputFile, input, (err) => {
